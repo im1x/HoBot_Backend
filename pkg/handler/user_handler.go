cb0c1e770d3f50516474e6b5b74354f50a09b003
Im1x <im1xdev@gmail.com>
2023-08-28 01:34:21 +0300

    Added simple validation

--- pkg/handler/user_handler.go
+++ pkg/handler/user_handler.go
@@ -3,16 +3,23 @@ package handler
 import (
 	"HoBot_Backend/pkg/model"
 	usetService "HoBot_Backend/pkg/service"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"time"
 )
+
+var validate = validator.New()
 
 func Register(c *fiber.Ctx) error {
 	user := new(model.User)
 
 	if err := c.BodyParser(&user); err != nil {
 		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	if err := validate.Struct(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
 	}
 
 	res, err := usetService.Registration(*user)
--- pkg/model/user.go
+++ pkg/model/user.go
@@ -1,11 +1,13 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
 	Id          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Login       string             `bson:"login,omitempty" json:"login,omitempty"`
-	Password    string             `bson:"password,omitempty" json:"password,omitempty"`
+	Login       string             `bson:"login,omitempty" json:"login,omitempty" validate:"required"`
+	Password    string             `bson:"password,omitempty" json:"password,omitempty" validate:"required,gte=8"`
 	Channel     string             `bson:"channel,omitempty" json:"channel,omitempty"`
 	IsConfirmed bool               `bson:"confirmed,omitempty" json:"confirmed,omitempty"`
 }
