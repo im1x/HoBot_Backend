be4597f32a9b5ad4ac6befd0b132bb8073a7bc96
Im1x <im1xdev@gmail.com>
2024-05-07 22:14:15 +0300

    Add skip ordering of songs that can't be played

--- pkg/handler/songRequest_handler.go
+++ pkg/handler/songRequest_handler.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"HoBot_Backend/pkg/service/chat"
 	"HoBot_Backend/pkg/service/songRequest"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 	"net/url"
 )
 
@@ -37,6 +40,23 @@ func Playlist(c *fiber.Ctx) error {
 
 func SkipSong(c *fiber.Ctx) error {
 	userId := parseUserIdFromRequest(c)
+
+	if len(c.BodyRaw()) > 2 {
+		type AutoSkip struct {
+			AutoSkip bool `json:"autoSkip"`
+		}
+		var autoSkip AutoSkip
+		err := c.BodyParser(&autoSkip)
+		if err == nil && autoSkip.AutoSkip {
+			song, err := songRequest.GetCurrentSong(userId)
+			if err != nil {
+				log.Error("Error while getting current song:", err)
+				return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+			}
+			chat.SendMessageToChannel(fmt.Sprintf("Заказ \"%s\" от %s не может быть воспроизведен, пропущен.", song.Title, song.By), userId, nil)
+		}
+	}
+
 	err := songRequest.SkipSong(userId)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
