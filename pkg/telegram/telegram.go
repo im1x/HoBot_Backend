4046804376998c2dde681d08fbb6e0d03b5bcd58
Im1x <im1xdev@gmail.com>
2025-01-25 23:51:49 +0300

    Fix🛠️(Telegram): sending multiline messages

--- pkg/telegram/telegram.go
+++ pkg/telegram/telegram.go
@@ -3,15 +3,17 @@ package telegram
 import (
 	"github.com/gofiber/fiber/v2/log"
 	"net/http"
+	"net/url"
 	"os"
 )
 
 func SendMessage(msg string) {
 	go func() {
+		encodedMsg := url.QueryEscape(msg)
 		_, err := http.Post("https://api.telegram.org/bot"+
 			os.Getenv("TELEGRAM_BOT_TOKEN")+
 			"/sendMessage?chat_id=-4743294690&text="+
-			msg, "application/json", nil)
+			encodedMsg, "application/json", nil)
 		if err != nil {
 			log.Error("Error while sending message to telegram:", err)
 			return
